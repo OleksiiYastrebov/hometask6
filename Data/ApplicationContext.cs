ce358d6cdd2a87f268f6d41f81d2559c5d2730e4
Oleksii Yastrebov <yastrebov.oleksii@gmail.com>
2025-07-14 18:01:42 +0300

    Implemented Expense model, ApplicationContext

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using hometask6.Models;
+
+namespace hometask6.Data;
+
+public class ApplicationContext: DbContext
+{
+    public DbSet<Expense> Expenses { get; set; }
+
+    public ApplicationContext(DbContextOptions options) : base(options) {}
+}
---
+++ Models/Expense.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace hometask6.Models;
+
+public class Expense
+{
+    public enum ExpenseCategory
+    {
+        Food,
+        Transport,
+        MobileCommunication,
+        Internet,
+        Entertainment
+    }
+
+    [Key] public Guid Id { get; set; }
+
+    [Range(0, 4, ErrorMessage = "Value must be in range [0, 4]")]
+    public ExpenseCategory Category { get; set; }
+
+    [Column(TypeName = "decimal(8,2)")]
+    [Range(0.01, 999999.99, ErrorMessage = "Value must be in range [0.01-999999.99]")]
+    public decimal Price { get; set; }
+
+    [Column(TypeName = "nvarchar(255)")] public string? Comment { get; set; }
+}
