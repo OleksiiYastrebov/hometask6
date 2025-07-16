bf7dac4fcb859c601d03a4cb2884aa819f1e0cd3
Oleksii Yastrebov <yastrebov.oleksii@gmail.com>
2025-07-16 12:57:55 +0300

    Created CategorySeeder.cs updated Program.cs to use seeder and use scalar

---
+++ Data/CategorySeeder.cs
@@ -0,0 +1,25 @@
+﻿using hometask6.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace hometask6.Data;
+
+public class CategorySeeder
+{
+    public static async Task SeedAsync(ApplicationContext context)
+    {
+        if (!await context.Categories.AnyAsync())
+        {
+            var defaultCategories = new[]
+            {
+                new Category { Id = Guid.CreateVersion7(), Name = "Food" },
+                new Category { Id = Guid.CreateVersion7(), Name = "Transport" },
+                new Category { Id = Guid.CreateVersion7(), Name = "Mobile" },
+                new Category { Id = Guid.CreateVersion7(), Name = "Internet" },
+                new Category { Id = Guid.CreateVersion7(), Name = "Entertainment" }
+            };
+
+            context.Categories.AddRange(defaultCategories);
+            await context.SaveChangesAsync();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
+using FluentValidation;
 using hometask6.Data;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionStr = builder.Configuration.GetConnectionString("Default");
@@ -7,17 +9,21 @@ var connectionStr = builder.Configuration.GetConnectionString("Default");
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connectionStr));
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationContext>();
+    await CategorySeeder.SeedAsync(dbContext);
+}
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
