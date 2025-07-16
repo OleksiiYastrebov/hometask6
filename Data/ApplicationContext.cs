a501f008e503993f15bfa3a4e6f7a90dd1618dcd
Oleksii Yastrebov <yastrebov.oleksii@gmail.com>
2025-07-16 12:55:55 +0300

    Created ApplicationContext.cs

--- Data/ApplicationContext.cs
+++ Data/ApplicationContext.cs
@@ -1,11 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
-using hometask6.Models;
+﻿using hometask6.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace hometask6.Data;
 
-public class ApplicationContext: DbContext
+public class ApplicationContext : DbContext
 {
-    public DbSet<Expense> Expenses { get; set; }
+    public ApplicationContext(DbContextOptions options) : base(options)
+    {
+    }
 
-    public ApplicationContext(DbContextOptions options) : base(options) {}
+    public DbSet<Expense> Expenses { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Expense>()
+            .HasOne(e => e.Category)
+            .WithMany(c => c.Expenses)
+            .HasForeignKey(e => e.CategoryId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
 }
