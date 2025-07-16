1db649fd9bbe67a916ce9326d5e9762a3020a3c9
Oleksii Yastrebov <yastrebov.oleksii@gmail.com>
2025-07-16 12:54:26 +0300

    Created models

---
+++ Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace hometask6.Models;
+
+public class Category
+{
+    public Guid Id { get; set; }
+
+    [Column(TypeName = "nvarchar(100)")] 
+    public string Name { get; set; } = null!;
+
+    public ICollection<Expense>? Expenses { get; set; } = new List<Expense>();
+}
--- Models/Expense.cs
+++ Models/Expense.cs
@@ -1,27 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace hometask6.Models;
 
 public class Expense
 {
-    public enum ExpenseCategory
-    {
-        Food,
-        Transport,
-        MobileCommunication,
-        Internet,
-        Entertainment
-    }
+    public Guid Id { get; set; }
+    public Guid CategoryId { get; set; }
 
-    [Key] public Guid Id { get; set; }
-
-    [Range(0, 4, ErrorMessage = "Value must be in range [0, 4]")]
-    public ExpenseCategory Category { get; set; }
-
-    [Column(TypeName = "decimal(8,2)")]
-    [Range(0.01, 999999.99, ErrorMessage = "Value must be in range [0.01-999999.99]")]
+    [Column(TypeName = "decimal(10,2)")] 
     public decimal Price { get; set; }
 
-    [Column(TypeName = "nvarchar(255)")] public string? Comment { get; set; }
+    [Column(TypeName = "nvarchar(255)")] 
+    public string? Comment { get; set; }
+
+    public Category Category { get; set; } = null!;
 }
